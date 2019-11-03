5eca8576fd4878c48039a5b83bd65c441535a706
Alisa :) <44328221+dtxals@users.noreply.github.com>
2019-11-03 11:34:11 -0600

    push

--- city_information.py
+++ city_information.py
@@ -23,11 +23,14 @@ def get_article(city : str):
     wait = WebDriverWait(session, 3)
     session.get(website)
 
+    # gets the link of the article 
     link = wait.until(EC.element_to_be_clickable((By.PARTIAL_LINK_TEXT, keyword)))
     article = link.get_attribute('href')
+    print(article)
     session.close()
     return article
 
+# web scrapes and gets the top list of activities to do in city
 def get_activities(article : str):
     html = urllib.request.urlopen(article)
     soup = BeautifulSoup(html, 'lxml')
@@ -39,8 +42,27 @@ def get_activities(article : str):
         activities.append(tag)
     return activities
 
+# creates a csv file of activity list
 def activities_to_csv(activities : list):
     with open('activities.csv', 'w') as activityFile:
         for activity in activities:
             activityFile.write(activity + '\n')
     activityFile.close()
+    print('Done. Check the activities.csv')
+
+cities = []
+with open('matches.csv','r') as cityFile:
+    cityFile.readline()
+    for line in cityFile:
+        line = line.replace('\n', '').split(',')
+        cities += [line[1]]
+cities = set(cities)
+print(cities)
+with open('city_info.txt', 'w') as cityInfo:
+    for city in cities:
+        print(city)
+        cityInfo.write(f'{city.title()}\n')
+    
+        for activity in get_activities(get_article(city)):
+            cityInfo.write(f'- {activity}\n')
+        cityInfo.write('\n')
