b96b5a925a0063365de37ea99bff1da534a0bbca
Josue Rosales <jrosa1756@gmail.com>
2019-11-03 09:09:47 -0600

    Add files via upload
    Added more city options

--- HackTX-2019/scripts/scriptQ.js
+++ HackTX-2019/scripts/scriptQ.js
@@ -1,16 +1,15 @@
 function getInput() {
-    var city = document.getElementById("city").value;
+    var city1 = document.getElementById("city1").value;
+    var city2 = document.getElementById("city2").value;
+    var city3 = document.getElementById("city3").value;
     var money = document.getElementById("money").value;
     var sex = document.getElementById("sex").value;
 
 
-    if(city == "" || money=="Choose One" || sex == "Choose One") {
+    if(city1 == "" || city2 == "" || city3 == "" || money=="Choose One" || sex == "Choose One") {
         window.alert("ERROR: No valid input!");
     } else {
-        console.log(city);
-        console.log(money);
-        console.log(sex);
-        var myData = new Array(city, money, sex);
+        var myData = new Array(city1, city2, city3, money, sex);
 
         $.ajax
         ({
